b43bd3c3acc06be325ec4c3fa7244aa1dfec9a3f
Dilan <dilannebioglu@gmail.com>
2017-10-31 00:13:22 +0000

    react intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
+import Message from './Message.jsx';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      // <h1>Hello React :)</h1>
+      <body>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <MessageList>
+          <Message />
+        </MessageList>
+        <ChatBar />
+      </body>
+
     );
   }
 }
---
+++ src/Message.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+class Message extends Component {
+  render() {
+    return (
+      <span>
+        <div className = "message" >
+          <span className="message-username">
+            Anonymous1
+          </span>
+          <span className = "message-content" >
+            I won &#39;t be impressed with technology until I can download food.
+          </span>
+         </div>
+         <div className="message system">
+           Anonymous1 changed their name to nomnom.
+         </div>
+       </span>
+    );
+  }
+}
+export default Message;
